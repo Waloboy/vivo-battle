42d9ae076e27fe7a971b04c21b2dbd4513795774
Eduardo Avila <eduardovsqz30@gmail.com>
2026-05-08 20:05:34 -0400

    Sin vercel.json

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.10.123', 'localhost:3000'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; script-src * 'unsafe-inline' 'unsafe-eval' 'self'; connect-src * 'unsafe-inline' wss: ws:; img-src * data: blob:; frame-src *; style-src * 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
